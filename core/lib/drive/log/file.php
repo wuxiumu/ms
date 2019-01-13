e44e1a45b320e4247a3518fec89709b0e8bb22b2
wqb <meiyoufan@outlook.com>
2019-01-13 20:12:03 +0800

    markdow style css

--- core/lib/drive/log/file.php
+++ core/lib/drive/log/file.php
@@ -16,11 +16,11 @@ class file
 		 * 2.新建目录	
 		 * 3.写入日志
 		 */		
-		$path = conf::get('OPTION','log');		
-		if(!is_dir($this->path.date("YmdH",time()))){			
-			mkdir($this->path.date("YmdH",time()),'0777',true);
-		}		
-		return file_put_contents($this->path.date("YmdH",time()).DIRECTORY_SEPARATOR.$file.'.php', date("Y-m-d H:i:s").json_encode($msg).PHP_EOL,FILE_APPEND);
+		// $path = conf::get('OPTION','log');		
+		// if(!is_dir($this->path.date("YmdH",time()))){			
+		// 	mkdir($this->path.date("YmdH",time()),'0777',true);
+		// }		
+		// return file_put_contents($this->path.date("YmdH",time()).DIRECTORY_SEPARATOR.$file.'.php', date("Y-m-d H:i:s").json_encode($msg).PHP_EOL,FILE_APPEND);
 	}
 }
 //文件系统
