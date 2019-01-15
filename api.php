5ac4745de3557f640da2424fce60a0dff87c92c8
wqb <meiyoufan@outlook.com>
2019-01-15 23:26:12 +0800

    添加协议，部分更新

--- api.php
+++ api.php
@@ -18,7 +18,51 @@ define('PHPMSFRAME',__DIR__);                    //当前内容写在哪个文�
 define('CORE',PHPMSFRAME.'/core');
 
 require CORE.'/flight/Flight.php';
+require PHPMSFRAME.'/nosql/Redis.php';
+
 Flight::route('/api.php', function(){
-	echo '一个会飞的php微型框架';    
+	 // $new = new Redisengine;
+	 // //$new->sscache();
+	 // $new->olfst();
+	 test2();
 });
 Flight::start();
+
+function test2(){
+	echo 1;
+}
+function test1(){
+	$redis = new RedisClusteri;
+
+	$redis->connect(array('host'=>'127.0.0.1','port'=>6379));
+
+
+	//*
+	$cron_id = 10001;
+	$CRON_KEY = 'CRON_LIST'; //
+	$PHONE_KEY = 'PHONE_LIST:'.$cron_id;//
+
+	//cron info
+	$cron = $redis->hget($CRON_KEY,$cron_id);
+	if(empty($cron)){
+
+	  $cron = array('id'=>10,'name'=>'jackluo');//mysql data
+	  $redis->hset($CRON_KEY,$cron_id,$cron); // set redis
+	}
+	//phone list
+	$phone_list = $redis->lrange($PHONE_KEY,0,-1);
+	print_r($phone_list);
+	if(empty($phone_list)){
+	  $phone_list =explode(',','13228191831,18608041585');    //mysql data
+	  //join  list
+	  if($phone_list){
+	    $redis->multi();
+	    foreach ($phone_list as $phone) {
+	      $redis->lpush($PHONE_KEY,$phone);
+	    }
+	    $redis->exec();
+	  }
+	}
+
+	print_r($phone_list);
+}
---
+++ w_start_web.bat
@@ -0,0 +1,2 @@
+@echo off
+composer update && php -S localhost:8000
