f8872794b255938f7240dba0ffe64b24ff9e98d6
wqb <meiyoufan@outlook.com>
2019-01-19 13:12:05 +0800

    吧数据驱动分开，可根据需要自己配置

--- core/lib/model.php
+++ core/lib/model.php
@@ -2,34 +2,9 @@
 
 namespace core\lib;
 
-use \core\lib\conf;
+use  core\lib\drive\database\medooModel;
 
-use Medoo\Medoo;
-
-class model extends Medoo
+class model extends medooModel
 {	
-	public function __construct()
-	{
-		$option = conf::all('database');		
-		parent::__construct($option['mysql_medoo_conf']);
-	}
+	
 }
-/**************************Medoo拓展**************************/
-
-/**************************pdo拓展**************************/
-// class model extends \PDO
-// {	
-// 	public function __construct()
-// 	{
-// 		$database = conf::all('database');	
-// 		$dsn='mysql:host='.$database['msyql_default']['DSN'].';dbname='.$database['msyql_default']['DBNAME'];
-// 		$username=$database['msyql_default']['USERNAME'];
-// 		$password=$database['msyql_default']['PASSWORD'];
-// 		try{
-// 			parent::__construct($dsn,$username,$password);
-// 		}catch(\PDOException $e){
-// 			p($e->getMessage());
-// 		}
-// 	}
-// }
-/**************************pdo拓展end**************************/
